04d4cbf16621e086e9517eb10a25738909b9d30e
BOWU115 <3302047784@qq.com>
2025-07-16 14:50:58 +0800

    测试BOWU邮箱

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// 点击图片，切换
 const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
@@ -28,16 +29,31 @@ if (!localStorage.getItem("name")) {
   myHeading.textContent = `Mozilla i cool, ${storedName}`;
 }
 
+// 点击按钮，切换角色
 myButton.onclick = function () {
   setUserName();
 };
 
-const myImage = document.querySelector("img");
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/PIcture.jpg") {
-    myImage.setAttribute("src", "images/loginPicture.jpg");
-  } else {
-    myImage.setAttribute("src", "images/PIcture.jpg");
-  }
-};
+// const list = document.createElement("ul");
+// const info = document.createElement("p");
+// const ul = document.querySelector("ul");
+
+// info.textContent =
+//   "Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.";
+
+// document.body.appendChild(info);
+// document.body.appendChild(list);
+
+// ul.onclick = function () {
+//   const listItem = document.createElement("li");
+//   const text = prompt("Enter the text for the new list item:");
+
+//   listItem.textContent = text;
+//   list.appendChild(listItem);
+
+//   listItem.onclick = function (event) {
+//     event.stopPropagation(); // Prevent the click from bubbling up to the ul element
+//     const newText = prompt("Enter the new text for this list item:");
+//     this.textContent = newText;
+//   };
+// };
